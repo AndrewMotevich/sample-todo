d65e8b40618f4ae8c2602c8d1ee1f3403960ad68
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-06-19 15:11:29 +0300

    feat: add theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ThemeService } from './theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   @Input() title = 'Simple todo';
+
+  constructor(public themeService: ThemeService) {}
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme().then();
+  }
 }
