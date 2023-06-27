2c3a02ad25cd8e70edbbbc2b9b596fc04a0fea7a
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-06-27 16:23:26 +0300

    fix: fix not-found-page timer

--- src/app/todo/pages/not-found-page/not-found-page.component.ts
+++ src/app/todo/pages/not-found-page/not-found-page.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-not-found-page',
@@ -9,17 +8,10 @@ import { interval, take } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundPageComponent {
-  public intervalNumber = 5;
-  public interval = interval(1000).pipe(take(5));
-
   constructor(private changeDetection: ChangeDetectorRef, private router: Router) {
-    this.interval.subscribe(() => {
-      if (this.intervalNumber === 1) {
-        this.router.navigate(['/board']);
-      } else {
-        this.intervalNumber -= 1;
-        this.changeDetection.detectChanges();
-      }
-    });
+    setTimeout(() => {
+      this.router.navigate(['/board']);
+      this.changeDetection.detectChanges();
+    }, 5000);
   }
 }
